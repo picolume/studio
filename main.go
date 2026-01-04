5cd233deef0a6c2ecdcbfc49f441abf6f4d7271d
Brad Henson <henson@outlook.com>
2026-01-04 13:20:52 -0500

    updated logo svg

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed all:frontend
@@ -47,9 +48,10 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "PicoLume Studio",
-		Width:  1280,
-		Height: 800,
+		Title:   "",
+		Windows: &windows.Options{DisableWindowIcon: true},
+		Width:   1280,
+		Height:  800,
 		AssetServer: &assetserver.Options{
 			Assets: getAssets(),
 		},
